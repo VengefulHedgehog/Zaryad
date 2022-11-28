e225de2b23ab9297225e7d8158b0a5bbd9fc3d62
KuzminykhEM <kuzja-280599@mail.ru>
2022-11-28 14:48:06 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -3,7 +3,10 @@ $('.main__slider').slick({
     nextArrow: '.slider__arrow__next',
     centerMode: true,
     centerPadding: '40px',
-    slidesToShow: 1,
+    slidesToShow:1,
     autoplay: true,
     autoplaySpeed: 3000
 });
+
+const circle = document.querySelectorAll('#circle');
+circle.forEach(element => new CircleType(element));
