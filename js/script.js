5c8822340cc95a0e1d5a747484a274564fa5f411
KuzminykhEM <kuzja-280599@mail.ru>
2022-11-28 14:48:06 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -3,7 +3,10 @@ $('.main__slider').slick({
     nextArrow: '.slider__arrow__next',
     centerMode: true,
     centerPadding: '40px',
-    slidesToShow: 1,
-    autoplay: true,
-    autoplaySpeed: 3000
+    slidesToShow:1,
+    // autoplay: true,
+    // autoplaySpeed: 3000
 });
+
+
+document.querySelector('.circle').innerHTML = "СМОТРЕТЬ ОТЗЫВ КОНСТАНТИНА".split('').map((e,i) => `<span style="--rot:${i*13}deg">${e}</span>`).join('');
